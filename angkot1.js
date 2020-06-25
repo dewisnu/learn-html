1806a5f933ee3a71953dd3ff235f42e3f8f79cc3
ariwisnu <wisnuari4658@gmail.com>
2020-06-26 00:43:44 +0800

    mengganti var noangkot di bagian if dan menambahkan komentar

--- angkot1.js
+++ angkot1.js
@@ -5,7 +5,9 @@ var noangkot = 1;
 while (noangkot <= angkotberoperasi) {
     console.log('Angkot No. ' + noangkot + 'x beroperasi dengan baik')
 noangkot++    
-}for (; noangkot <= jmlangkot; noangkot++) {
+}
+//no angkot variable  disini dapat ditulikan seperti : noangkot ;,noangkot = angkotberoperasi + 1; , dikosongkan saja;. 
+for (noangkot; noangkot <= jmlangkot; noangkot++) {
     console.log('Angkot No. ' + noangkot + 'x sedang tidak beroperasi');
     
 }
