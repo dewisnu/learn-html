8cea03bf1e8abbb35bb27b2432b5eb34a4ad0a85
ariwisnu <wisnuari4658@gmail.com>
2020-06-26 00:26:13 +0800

    menambahkan file index html dan angkot1.js

---
+++ angkot1.js
@@ -0,0 +1,11 @@
+var jmlangkot = 10;
+var angkotberoperasi = 6;
+var noangkot = 1;
+
+while (noangkot <= angkotberoperasi) {
+    console.log('Angkot No. ' + noangkot + 'x beroperasi dengan baik')
+noangkot++    
+}for (; noangkot <= jmlangkot; noangkot++) {
+    console.log('Angkot No. ' + noangkot + 'x sedang tidak beroperasi');
+    
+}
