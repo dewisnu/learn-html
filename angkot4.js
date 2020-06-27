4bee9260c7959695952d914425d5f2e8c2945780
ariwisnu <wisnuari4658@gmail.com>
2020-06-27 14:46:42 +0800

    menambahkan logika atau (||) dipengkondisian else if di file angkot4.js

--- angkot4.js
+++ angkot4.js
@@ -5,9 +5,10 @@ var angkotberoperasi = 6;
 for (var noangkot = 1; noangkot <= jmlangkot; noangkot++) {
     if (noangkot <= 6) {
         console.log('Angkot No ' + noangkot + ' sedang beroperasi dengan baik');
-    } else if (noangkot === 8) {
+    } // (||) simbol ini artinya atau dalam operator logika
+    else if (noangkot === 8|| noangkot === 10) {
      console.log('Angkot No. ' + noangkot +' sedang lembur')   
-    }else (
+    } else (
         console.log('Angkot No ' + noangkot + ' sedang tidak beroperasi')
     )
 
