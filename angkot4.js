ecb7655bea169097e8552d31bbdcf9fd098176b3
ariwisnu <wisnuari4658@gmail.com>
2020-06-27 13:59:55 +0800

    menambahkan = di kondisi else if

--- angkot4.js
+++ angkot4.js
@@ -5,7 +5,7 @@ var angkotberoperasi = 6;
 for (var noangkot = 1; noangkot <= jmlangkot; noangkot++) {
     if (noangkot <= 6) {
         console.log('Angkot No ' + noangkot + ' sedang beroperasi dengan baik');
-    } else if (noangkot == 8) {
+    } else if (noangkot === 8) {
      console.log('Angkot No. ' + noangkot +' sedang lembur')   
     }else (
         console.log('Angkot No ' + noangkot + ' sedang tidak beroperasi')
