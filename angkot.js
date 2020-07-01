d83f68ad2e8501ef910d55b1cf57218dd90f36b5
ariwisnu <wisnuari4658@gmail.com>
2020-07-01 21:49:46 +0800

    memindahkan file .js ke folder js

--- angkot.js
+++
@@ -1,8 +0,0 @@
-var penentu =true;
- 
-
-while(penentu) {
-var nama = prompt('masukan nama');  
-  alert('selamat datang di website perangkotan duniawi ' + nama);
-    penentu =confirm('ingin mengulang lagi');
-}
