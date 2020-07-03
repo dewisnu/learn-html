75816e85b2b3b3839fddf40f0758dc379511b700
ariwisnu <wisnuari4658@gmail.com>
2020-07-04 00:18:39 +0800

    menambahkan comentar dan menambahkan perintah penggulangan

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var s = '';
+
+for (var i = 10; i > 0; i--){
+    for (var j = 0; j < i; j++) {
+        s += '*';
+        
+    }
+   s += '\n';
+}
+console.log(s);
+
+//loop pertama
+//10 lebih besar dari 0 = true 
+//kemudian masuk ke loop ke dua
+// 0 lebih kecil dari 10 = true 
+//cetak *  
+// masih di loop ke dua ( karena loop nya masih true )
+//var j + 1 = 1 ; 1 lebih kecil dari 10 = true 
+//cetak *
+//..
+//masih di loop ke dua ( karena loop nya masih bernilai  true)
+//var j + 11 = 11 ; 11 lebih kecil dari 10 = false (karena false looop kedua tidak dikerjaakan)
+//keluar dari loop kedua 
+// cetak baris baru / (\n)
+
+
